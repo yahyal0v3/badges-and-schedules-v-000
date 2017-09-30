c4f32f4c6cf7dfd3bacd832ba3fc0c20b0e397ef
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-09-30 18:44:25 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,9 +19,9 @@ end
 
 def printer(attendees)
   batch_badge_creator(attendees).each do |badge|
-    puts badge
+    badge
   end
   assign_rooms(attendees).each do |assignment|
-    puts assignment
+    assignment
   end
 end
